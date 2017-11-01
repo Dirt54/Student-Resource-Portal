b522badc67ac64f7560bd3efbf1d811c3347e257
Luke Robinson <lukerobinson@lukes-mbp.chattanoogatn.gov>
2017-11-01 17:07:34 -0400

    hv

--- client/controllers.js
+++ client/controllers.js
@@ -23,10 +23,7 @@ angular.module('portal.controllers', [])
         }
     
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 5dcc142d15cea9aac38792199e0b0372b1474abd
         $scope.signup = function () {
             var payload = {
                 firstname: $scope.firstname,
