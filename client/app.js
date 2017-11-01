7765f54429f3cb7ca249aa9fbd38c8c132fa880b
egarcia07x <31666808+egarcia07x@users.noreply.github.com>
2017-11-01 12:27:35 -0400

    aap.js

--- client/app.js
+++ client/app.js
@@ -6,23 +6,8 @@ angular.module('studentportal', ['ngRoute', 'ngResource', 'portal.controllers', 
         templateUrl: 'views/login.html',
         controller: 'loginController'
     })
-<<<<<<< HEAD
-  
-    .when('/loginredirect'{
-templateUrl: ' views/loginfail.html'
-
-    })  
-    .when ('/studentportal'{
-templateUrl: 'views/welcome.html',
-controller:''
-})
-
-
-
-=======
     .when('/welcome', {
         templateUrl: 'views/welcome.html',
         controller: 'welcomeController'
     })
->>>>>>> da3fe48b6256712f9fda090eb4a6dfca6d9f4175
 }]);
