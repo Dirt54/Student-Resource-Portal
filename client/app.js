4daa0b0c06315b9e3394af40d65d98aee11796ec
Luke Robinson <lukerobinson@lukes-mbp.chattanoogatn.gov>
2017-11-03 11:32:07 -0400

    chchchcanges

--- client/app.js
+++ client/app.js
@@ -1,47 +1,61 @@
 angular.module('studentportal', ['ngRoute', 'ngResource', 'portal.controllers', 'portal.factories', 'portal.services'])
-.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-    $routeProvider
-    .when('/', {
-        templateUrl: 'views/login.html',
-        controller: 'loginController'
-    })
-    .when('/loginfail', {
-        templateUrl: 'loginfail.html'
-    })
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/login.html',
+                controller: 'loginController'
+            })
 
-    // when('/logout', {
+            .when('/welcome', {
+                templateUrl: 'views/welcome.html',
+                controller: 'welcomeController',
+                requiresLogin: true,
+            })
 
-    //     controller: 'logoutController'
+            .when('/users', {
+                templateUrl: 'views/users.html',
+                controller: 'usersController',
+                requiresLogin: true,
+                requiresAdmin: true,
+                requiresActive: true
+            })
 
-    // })
-    .when('/welcome', {
-        templateUrl: 'views/welcome.html',
-        controller: 'welcomeController'
+            .when('/resources', {
+                templateUrl: 'views/resources.html',
+                controller: 'resourcesController',
+                requiresLogin: true,
+                requiresActive: true
+            })
 
-    })
-   
-    .when('/users', {
-        templateUrl: 'views/users.html',
-        controller: 'usersController'
-    })
-   
-    .when('/resources', {
-        templateUrl: 'views/resources.html',
-        controller: 'resourcesController'
-    })
-    
-    .when('/syllabus', {
-        templateUrl: 'views/syllabus.html',
-        controller: 'syllabusController'
-    })
-    .when('/lectures', {
-        templateUrl: 'views/lectures.html',
-        controller: 'LecturesController'
-    })
-    .when('/labs', {
-        templateUrl: 'views/labs.html',
-        controller: 'labsController'
-    })
-
-}]);
+            .when('/syllabus', {
+                templateUrl: 'views/syllabus.html',
+                controller: 'syllabusController',
+                requiresLogin: true,
+            })
+            .when('/lectures', {
+                templateUrl: 'views/lectures.html',
+                controller: 'LecturesController',
+                requiresLogin: true,
+                requiresActive: true
+            })
+            .when('/labs', {
+                templateUrl: 'views/labs.html',
+                controller: 'labsController',
+                requiresLogin: true,
+                requiresActive: true
+            })
+    }])
+    .run(['$rootScope', '$location', 'UserService', function ($rootScope, $location, UserService) {
+        $rootScope.$on('$routeChangeStart', function (event, nextRoute, previousRoute) {
+            if (nextRoute.$$route.requiresLogin && !UserService.isLoggedIn()) {
+                event.preventDefault();
+                UserService.loginRedirect();
+            } else if (nextRoute.$$route.requiresAdmin && !UserService.isAdmin()) {
+                event.preventDefault();
+                $location.replace().path('/welcome');
+            } else if (nextRoute.$$route.requiresActive && !UserService.isActive()) {
+                $location.replace().path('/welcome');
+            }
+        });
+    }]);
