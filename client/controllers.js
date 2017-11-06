aa034d199ab846a1fdce35c9267a7d8ecf69940a
antmedina <ant.n.medina@gmail.com>
2017-11-06 11:24:23 -0500

    add new syllabus pages and connectivity

--- client/controllers.js
+++ client/controllers.js
@@ -205,13 +205,10 @@ angular.module('portal.controllers', [])
 
 
 .controller('syllabusController', ['$scope', '$location', '$routeParams', 'UserService', 'SEOService', function ($scope,  $location, $routeParams, UserService, SEOService) {
+}])
+
+.controller('frontendsyllabusController', ['$scope', '$location', '$routeParams', 'UserService', 'SEOService', function ($scope,  $location, $routeParams, UserService, SEOService) {
+}])
     
-    // $('a[href^="#"]').on('click', function (e) {
-    //     e.preventDefault();
-    //     var target = this.hash;
-    //     var $target = $(target);
-    //     $('html, body').stop().animate({
-    //         scrollTop: $target.offset().top
-    //     }, 1500, 'swing');
-    // });
+.controller('reactsyllabusController', ['$scope', '$location', '$routeParams', 'UserService', 'SEOService', function ($scope,  $location, $routeParams, UserService, SEOService) {
 }]);
